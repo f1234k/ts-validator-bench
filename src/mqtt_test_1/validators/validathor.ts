bd7db75611fd9321d82b67ec53b5b3f4a0fd18ea
Thomas Minitsios <thomas@minitsios.com>
2025-07-10 11:03:55 +0200

    Add v.literal for validathor with typescript errors

--- src/mqtt_test_1/validators/validathor.ts
+++ src/mqtt_test_1/validators/validathor.ts
@@ -3,16 +3,11 @@ import * as v from "@nordic-ui/validathor";
 import { json_safe_parse } from "../../helpers/utils";
 import type {
   ErrorCounter,
-  MqttButtonAdv1,
-  MqttButtonAdv4,
-  MqttButtonAdv8,
-  MqttCallback,
-  MqttGatewayAdvData,
-  MqttGatewayAlive,
+  MqttCallback
 } from "../types";
 
 const MqttButtonAdv1SchemaValidathor = v.object({
-  type: v.number(),
+  type: v.literal(1),
   dmac: v.string(),
   time: v.string(),
   rssi: v.number(),
@@ -27,7 +22,7 @@ const MqttButtonAdv1SchemaValidathor = v.object({
 });
 
 const MqttButtonAdv4SchemaValidathor = v.object({
-  type: v.number(),
+  type: v.literal(4),
   dmac: v.string(),
   uuid: v.string(),
   majorID: v.number(),
@@ -38,7 +33,7 @@ const MqttButtonAdv4SchemaValidathor = v.object({
 });
 
 const MqttButtonAdv8SchemaValidathor = v.object({
-  type: v.number(),
+  type: v.literal(8),
   dmac: v.string(),
   vbatt: v.number(),
   temp: v.number(),
@@ -49,7 +44,7 @@ const MqttButtonAdv8SchemaValidathor = v.object({
 });
 
 const MqttGatewayAliveSchemaValidathor = v.object({
-  msg: v.string(),
+  msg: v.literal("alive"),
   gmac: v.string(),
   ver: v.string(),
   subaction: v.string(),
@@ -76,7 +71,7 @@ const MqttBeaconMessageSchemaValidathor = v.union([
 ]);
 
 const MqttGatewayAdvDataSchemaValidathor = v.object({
-  msg: v.string(),
+  msg: v.literal("advData"),
   gmac: v.string(),
   obj: v.array(MqttBeaconMessageSchemaValidathor),
 });
@@ -109,21 +104,21 @@ export async function process_validathor(
 
   const validatedPayload = result;
   if (validatedPayload.msg === "advData") {
-    const payload_adv = validatedPayload as MqttGatewayAdvData;
+    const payload_adv = validatedPayload ;
     payload_adv.obj.forEach((obj) => {
       if (obj.type === 1) {
-        callback(payload_adv.gmac, "adv1", obj as MqttButtonAdv1);
+        callback(payload_adv.gmac, "adv1", obj);
       } else if (obj.type === 4) {
-        callback(payload_adv.gmac, "adv4", obj as MqttButtonAdv4);
+        callback(payload_adv.gmac, "adv4", obj );
       } else if (obj.type === 8) {
-        callback(payload_adv.gmac, "adv8", obj as MqttButtonAdv8);
+        callback(payload_adv.gmac, "adv8", obj);
       }
     });
   } else if (validatedPayload.msg === "alive") {
     callback(
       validatedPayload.gmac,
       "alive",
-      validatedPayload as MqttGatewayAlive
+      validatedPayload
     );
   }
 }
